6bf84a99c9fd4fe2b1127057562e1c7393ee48e3
YoYo2201 <manas.agrawal1422@gmail.com>
2022-09-18 02:37:26 +0530

    Files Updated

--- Backend/server.js
+++ Backend/server.js
@@ -17,9 +17,9 @@ const server = http.createServer(app, {
 const PORT = process.env.PORT || 4000;
 if(process.env.NODE_ENV === "production")
 {
-  app.use(express.static('build'));
+  app.use(express.static(path.resolve(process.cwd(), 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(process.cwd(), 'build/index.html'));
   });
 }
 
